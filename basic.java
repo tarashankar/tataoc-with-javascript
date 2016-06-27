1e47e99c6505eb71da1aa96ffd980960724d4fa3
tarashankar <gtataracity@gmail.com>
2016-06-27 15:16:32 +0530

    Add files via upload

--- basic.java
+++ basic.java
@@ -1,77 +1,72 @@
-package tatocwithoutfindelement;
+package tatocusingjs;
+
+ import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import com.thoughtworks.selenium.Selenium;
-import com.thoughtworks.selenium.webdriven.WebDriverBackedSelenium;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.JavascriptExecutor;
 
+public class basic 
+{
 
-@SuppressWarnings("deprecation")
-public class basic {
-	public static void main(String args[]) throws InterruptedException {
-		
+	
+	public static void main(String[] args) throws Exception
+	{
 		WebDriver driver = new FirefoxDriver();
-
-		// A "base url", used by selenium to resolve relative URLs
-		 String baseurl = "http://10.0.1.86";
-
-		// Create the Selenium implementation
-		@SuppressWarnings("deprecation")
-		Selenium selenium = new WebDriverBackedSelenium(driver, baseurl);
+	    driver.get("http://10.0.1.86/tatoc/basic/grid/gate");
+	    driver.manage().window().maximize();
+	    JavascriptExecutor js = (JavascriptExecutor) driver;
+	        
+	    //Part 1
+        js.executeScript("document.getElementsByClassName('greenbox')[0].click();");//.click();
+        
+	    //Part 2
+	    String color1 = (String) js.executeScript("return document.querySelector('#main').contentWindow.document.querySelector('#answer').className;");
+		String color2 = (String) js.executeScript("return document.querySelector('#main').contentWindow.document.querySelector('#child').contentWindow.document.getElementById('answer').getAttribute('class');");
+		Thread.sleep(500);
+		while(!color1.equals(color2))
+		{
+			Thread.sleep(500);
+			js.executeScript("document.getElementById('main').contentWindow.document.getElementsByTagName('a')[0].click();");
+			Thread.sleep(350);
+			color2=(String)js.executeScript("return document.querySelector('#main').contentWindow.document.querySelector('#child').contentWindow.document.getElementById('answer').getAttribute('class');");
+		}		
+		js.executeScript("document.getElementById('main').contentWindow.document.getElementsByTagName('a')[1].click();");
 		
-		selenium.open(baseurl);
-		
-		selenium.click("//a[contains(text(),'/tatoc')]");
-		selenium.click("//a[contains(text(),'Basic Course')]");
-		selenium.click("class=greenbox");
-		String s1 = selenium.getAttribute("//div[(@id='answer')]@class");
-		System.out.println(s1);
-		selenium.selectFrame("main");
-		selenium.selectFrame("child");
-		String s2 = selenium.getAttribute("//div[(@id='answer')]@class");
-		System.out.println(s2);
-		while(true) {
-			
-			if(s1.equals(s2)) break;
-			else
-			{
-				selenium.selectFrame("relative=up");
-				selenium.click("//a[contains(text(),'Repaint Box 2')]");
-				
-			}
-			
-			selenium.selectFrame("child");
-			s2 = selenium.getAttribute("//div[(@id='answer')]@class");
-		}
-		selenium.selectFrame("relative=up");
-		selenium.click("//a[contains(text(),'Proceed')]");
-		
-		selenium.dragAndDropToObject("//div[(@id='dragbox')]", "//div[(@id='dropbox')]");
-		selenium.click("//a[contains(text(),'Proceed')]");
-		
-		
-		//popup handle
-		selenium.click("//a[contains(text(),'Launch Popup Window')]");
-		//selenium.selectWindow("Popup - Basic Course - T.A.T.O.C");
-		selenium.selectPopUp("Popup - Basic Course - T.A.T.O.C");
-		//selenium.windowFocus();
-		System.out.println("chala gaya");
-		//Thread.sleep(3000); 
-		selenium.click("//input[(@id='name')]");
-		selenium.type("//input[(@id='name')]","tara");
-		selenium.click("//input[(@id='submit')]");
-		System.out.println("aa gaya");		
-		selenium.selectWindow(null);
-		selenium.click("//a[contains(text(),'Proceed')]");
-		
-		
-		//cookie
-		selenium.click("//a[contains(text(),'Generate Token')]");
-		String token = selenium.getText("id=token");
-		token = token.substring(7);
-		selenium.createCookie("Token="+token+"","path=http://10.0.1.86/tatoc/basic/cookie, max_age=60");
-		selenium.click("//a[contains(text(),'Proceed')]");
-				
-
+		//Part 3
+		WebElement element1 = (WebElement)js.executeScript("return document.getElementById('dragbox')");
+//		System.out.println(element1);
+	    WebElement target = (WebElement)js.executeScript("return document.getElementById('dropbox')");
+	    new Actions(driver).dragAndDrop(element1, target).perform();
+        Thread.sleep(500);
+        js.executeScript("$('a:contains(\"Proceed\")').trigger('click')");  
+	    
+        //Part 4
+	    String mainwindow=driver.getWindowHandle();
+	    js.executeScript("$('a:contains(\"Launch Popup Window\")').trigger('click')");
+	    for(String winHandle :driver.getWindowHandles())
+	    {
+	        driver.switchTo().window(winHandle);
+	        if(driver.getTitle().equals("Popup - Basic Course - T.A.T.O.C"))
+	        {
+	            break;
+	        }
+	    }
+	    js.executeScript("document.getElementById(\"name\").value=\"tara\";");
+	    Thread.sleep(500);
+	    js.executeScript("document.getElementById('submit').click();");
+        driver.switchTo().window(mainwindow);
+	    js.executeScript("$('a:contains(\"Proceed\")').trigger('click')");    
+	    
+	    //Part 5
+	    js.executeScript("$('a:contains(\"Generate Token\")').trigger('click')");
+	    String s3 = (String)js.executeScript("return document.getElementById('token').textContent;");
+		s3 = "Token="+s3.substring(7,s3.length());
+		Cookie name = new Cookie("mycookie",s3);
+		driver.manage().addCookie(name);
+		js.executeScript("$('a:contains(\"Proceed\")').trigger('click')");    
+	    
+	    driver.close();
 	}
-
 }
